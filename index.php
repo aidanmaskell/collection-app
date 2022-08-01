7075304ffa0a16316123a6601c397dcb0f942a1c
aidanmaskell <aidan.maskell@gmail.com>
2022-08-01 15:23:28 +0100

    formed structure and minor css additions

--- index.php
+++ index.php
@@ -5,13 +5,33 @@ $db = getdb();
 $chilliData = collectDBData($db);
 
 ?>
-<html>
-    <head>
 
-    </head>
+<!DOCTYPE html>
+<html lang="en-gb">
+<head>
+	<title>Chillis</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="normalize.css" type="text/css" rel="stylesheet" />
+	<link href="styles.css" type="text/css" rel="stylesheet" />
+</head>
     <body>
-        <table>
-            <?echo createTable($chilliData)?>
-        </table>
+        <header>
+                <h1>CHILLIS</h1>
+                <h2>Welcome to my chilli collection</h2>
+        </header>
+        <nav>
+            <div>
+                <!-- <form action='results.php' method='get' />
+                <label for='chillis'>Search for your favourite tennis player</label>
+                <input type='text' name='chillis' />
+                <input type='submit' />
+                </form> -->
+            </div>
+        </nav>
+        <main>
+            <table>
+                <?echo createTable($chilliData)?>
+            </table>
+        </main>
     </body>
 </html>
